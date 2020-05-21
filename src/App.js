9e919edfe9c6a9edbef19fed583c29ec75ca41f9
Andrei <andreiscorpion19892010@gmail.com>
2020-05-21 13:28:11 +0200

    create structure our project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Profiler } from 'react';
+import './App.css';
+import Header from './Components/Header/Header';
+import Navbar from './Components/Navbar/Navbar';
+import Content from './Components/Content/Content';
+import Footer from './Components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="app-container">
+     <Header />
+     <Navbar />
+     <Content />
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import  React  from 'react';
+import  styles  from './Navbar.module.css';
+
+const Navbar = () => {
+    return <div className={styles.navbar}>
+        <div>Profile</div>
+        <div>Messages</div>
+        <div>News</div>
+        <div>Music</div>
+        <div>Settings</div>
+    </div>
+}
+
+export default Navbar;
