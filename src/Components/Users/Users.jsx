0b25aa735dcf0957a34ddc5d87f25eae98df6527
Andrei <andreiscorpion19892010@gmail.com>
2020-06-15 14:47:47 +0200

    refactoring Users functional component to class component

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -3,33 +3,34 @@ import styles from './Users.module.css'
 import usersPhoto from "./../../images/usersPhoto.png";
 import * as axios from 'axios'
 
-const Users = (props) => {
 
-  if (props.users.length === 0) {
-
+class Users extends React.Component {
+  componentDidMount() {
     axios.get('https://social-network.samuraijs.com/api/1.0/users')
       .then(response => {
-        props.setUsers(response.data.items)
+        this.props.setUsers(response.data.items)
       })
   }
 
-  return (
-
-    <div>
-      {props.users.map(u => <div key={u.id} className={styles.userData}>
-        <div className={styles.photo}><img src={u.photos.small != null ? u.photos.small : usersPhoto} className={styles.userPhoto} /></div>
-        <div className={styles.button}>
-          {u.followed
-            ? <button onClick={() => { props.unFollow(u.id) }}>UnFollow</button>
-            : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+  render() {
+    return (
+      <div>
+        {this.props.users.map(u => <div key={u.id} className={styles.userData}>
+          <div className={styles.photo}><img src={u.photos.small != null ? u.photos.small : usersPhoto} className={styles.userPhoto} /></div>
+          <div className={styles.button}>
+            {u.followed
+              ? <button onClick={() => { this.props.unFollow(u.id) }}>UnFollow</button>
+              : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+          </div>
+          <div className={styles.name}>Name: {u.name}</div>
+          <div className={styles.status}>Status: {u.status}</div>
+          <div className={styles.country}>Country: 'There must be country name'</div>
+          <div className={styles.city}>City: 'There must be city name'</div>
         </div>
-        <div className={styles.name}>Name: {u.name}</div>
-        <div className={styles.status}>Status: {u.status}</div>
-        <div className={styles.country}>Country: 'There must be country name'</div>
-        <div className={styles.city}>City: 'There must be city name'</div>
+        )}
       </div>
-      )}
-    </div>
-  )
+    )
+  }
 }
+
 export default Users;
