9ae9c187b5522fa1d4c6140ceff8851e589a044c
Andrei <andreiscorpion19892010@gmail.com>
2020-06-25 22:36:43 +0200

    cookie, login, auth/me

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import News from "./Components/News/News";
@@ -10,13 +9,14 @@ import { Route } from "react-router-dom";
 import DialogsContainer from './Components/Dialogs/DialogsContainer';
 import UsersContainer from './Components/Users/UsersContainer';
 import ProfileContainer from './Components/Profile/ProfileContainer';
+import HeaderContainer from './Components/Header/HeaderContainer';
 
 
 const App = (props) => {
  
   return (
     <div className="app-container">
-      <Header />
+      <HeaderContainer />
       <Navbar />
       <div className="app-container-content">
         <Route path="/profile/:userId?" render={ () => <ProfileContainer />} />
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React  from 'react';
 import styles from './Header.module.css'
 import logo from './../../images/logo_2.png'
+import { NavLink } from 'react-router-dom';
 
 const Header = (props) => {
     return <div className={styles.header}>
         <img  src={logo} alt="logo"/>
+        
+        <div className = {styles.loginBlock}>
+            {props.isAuth ? props.login : <NavLink to={'/login'} >Login</NavLink>}
+        </div>
+
     </div>
 }
 
